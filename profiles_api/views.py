149d7271a21ccf032892dd1a79126ec2a305d5b1
Paras Lakhani <lakparas@gmail.com>
2022-12-15 01:33:14 +0530

    added view set to profiles api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import viewsets
+
+from profiles_api import serializers
 
 # Create your views here.
 
 class HelloApiView(APIView):
+    serializer_class =  serializers.HelloSerializer
 
     def get(self,request,format=None):
 
@@ -14,3 +19,51 @@ class HelloApiView(APIView):
             'is mapped manually to URLs'
         ]
         return Response({'message': 'Hello!','an_apiview':an_apiView})
+
+    def post(self,request):
+
+        serializer = self.serializer_class(data = request.data)
+        if(serializer.is_valid()):
+            name = serializer.validated_data.get("name")
+            message = f'hello {name}'
+            return(Response({'message':message}))
+        else:
+            return(Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST)
+                )
+
+
+class HelloViewSet(viewsets.ViewSet):
+    serializer_class =  serializers.HelloSerializer
+    def list(self,request):
+
+        a_viewset = [
+            'Uses actions (list,create,retrieve,update,partial_update)',
+            'Automatically maps to urls using routers',
+            'provide more functionality with more code'
+        ]
+        return(Response({'message':'Hello','a_viewset':a_viewset}))
+
+    def create(self,request):
+        serializer = self.serializer_class(data = request.data)
+        if(serializer.is_valid()):
+            name = serializer.validated_data.get('name')
+            message = f'hello {name}'
+            return(Response({'message':message}))
+        else:
+            return(Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST))
+
+    def retrieve(self,request,pk = None):
+        return(Response({'method':'get'}))
+
+    def update(self,request,pk=None):
+        return(Response({'method':'put'}))
+
+    def partial_update(self,request,pk=None):
+        return(Response({'method':'patch'}))
+ 
+    def destroy(self,request,pk=None):
+        return(Response({'method':'delete'}))
